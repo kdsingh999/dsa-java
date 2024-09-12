51a3ed9ca83da09f5f1b721256c2eeca0b456ae0
kd singh <rajputkdsingh999@gmail.com>
2024-09-12 22:48:46 +0530

    first commit

---
+++ src/DSA/main.java
@@ -0,0 +1,10 @@
+package DSA;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             System.out.println("Hello this is test");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module DSAwithJava {
+}
